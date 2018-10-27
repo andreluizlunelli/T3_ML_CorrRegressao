226bd8640e9ecfadf45f5c30c941732fc8a7139c
Karin Cristiane Corrêa <karincorrea12@gmail.com>
2018-10-26 21:17:02 -0300

    Funções de correlação e regressão

--- correlacao.m
+++ correlacao.m
@@ -1,2 +1,7 @@
-
-
+%
+function r = correlacao(x, y)
+  p1 = sum(((x-mean(x)) * (y-mean(y))));
+  p2 = ((sum(x-mean(x))^2) * (sum(y-mean(y))^2))
+  p2 = sqrt(p2);
+  r = p1 / p2;
+endfunction
--- regressao.m
+++ regressao.m
@@ -1,1 +1,7 @@
-
+%
+function c = regressao(x, y)
+  p1 = sum((x-mean(x)) * (y-mean(y))) / sum(x-mean(x))^2;
+  p0 = mean(y) - (p1 * mean(x));
+  
+  c = p0 + (p1 * x);  
+endfunction
